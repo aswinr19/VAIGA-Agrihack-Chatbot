50c662a3f064fb310fa1bed6369691a2af144b7b
aswinr <aswinr019@gmail.com>
2024-05-31 20:55:28 +0530

    new project set up using bun, vite , react and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl'>
+        Hello Vite :(
+     </div>
+   </>
+  )
+}
+
+export default App
--- vaiga-chat-app/chat_app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '4d292250a25b00be5627f191'
-
-from chat_app import routes
